f4468f12059eea892e31ed1e751a9b81128233ed
Nalin <nalinbaipluthong@gmail.com>
2023-11-17 17:25:39 +0700

    increase video frame for predict video feed cctv

--- object-detection-yolov8/ultralytics/yolo/v8/detect/predict.py
+++ object-detection-yolov8/ultralytics/yolo/v8/detect/predict.py
@@ -16,7 +16,7 @@ from collections import deque
 import numpy as np
 
 # Constants
-palette = (2 ** 11 - 1, 2 ** 15 - 1, 2 ** 20 - 1)
+palette = (2**11 - 1, 2**15 - 1, 2**20 - 1)
 line1 = [(895, 390), (1555, 490)]
 line2 = [(430, 325), (895, 390)]
 ppm = 8  # Pixels per meter
@@ -46,8 +46,10 @@ deepsort = DeepSort(
 
 def estimatespeed(Location1, Location2):
     # Euclidean Distance Formula
-    d_pixel = math.sqrt(math.pow(
-        Location2[0] - Location1[0], 2) + math.pow(Location2[1] - Location1[1], 2))
+    d_pixel = math.sqrt(
+        math.pow(Location2[0] - Location1[0], 2)
+        + math.pow(Location2[1] - Location1[1], 2)
+    )
     # defining thr pixels per meter
     d_meters = d_pixel / ppm
     # distance = speed/time
@@ -66,17 +68,19 @@ def init_tracker():
     #                     nms_max_overlap=cfg_deep.DEEPSORT.NMS_MAX_OVERLAP, max_iou_distance=cfg_deep.DEEPSORT.MAX_IOU_DISTANCE,
     #                     max_age=cfg_deep.DEEPSORT.MAX_AGE, n_init=cfg_deep.DEEPSORT.N_INIT, nn_budget=cfg_deep.DEEPSORT.NN_BUDGET,
     #                     use_cuda=True)
+
+
 ##########################################################################################
 
 
 def xyxy_to_xywh(*xyxy):
-    """" Calculates the relative bounding box from absolute pixel values. """
+    """ " Calculates the relative bounding box from absolute pixel values."""
     bbox_left = min([xyxy[0].item(), xyxy[2].item()])
     bbox_top = min([xyxy[1].item(), xyxy[3].item()])
     bbox_w = abs(xyxy[0].item() - xyxy[2].item())
     bbox_h = abs(xyxy[1].item() - xyxy[3].item())
-    x_c = (bbox_left + bbox_w / 2)
-    y_c = (bbox_top + bbox_h / 2)
+    x_c = bbox_left + bbox_w / 2
+    y_c = bbox_top + bbox_h / 2
     w = bbox_w
     h = bbox_h
     return x_c, y_c, w, h
@@ -108,7 +112,7 @@ def compute_color_for_labels(label):
     # elif label == 5:  # Bus
     #     color = (0, 149, 255)
     else:
-        color = [int((p * (label ** 2 - label + 1)) % 255) for p in palette]
+        color = [int((p * (label**2 - label + 1)) % 255) for p in palette]
     return tuple(color)
 
 
@@ -145,8 +149,9 @@ def draw_border(img, pt1, pt2, color, thickness, r, d):
 
 def UI_box(x, img, color=None, label=None, line_thickness=None):
     # Plots one bounding box on image img
-    tl = line_thickness or round(
-        0.002 * (img.shape[0] + img.shape[1]) / 2) + 1  # line/font thickness
+    tl = (
+        line_thickness or round(0.002 * (img.shape[0] + img.shape[1]) / 2) + 1
+    )  # line/font thickness
     color = color or [random.randint(0, 255) for _ in range(3)]
     c1, c2 = (int(x[0]), int(x[1])), (int(x[2]), int(x[3]))
     cv2.rectangle(img, c1, c2, color, thickness=tl, lineType=cv2.LINE_AA)
@@ -154,11 +159,26 @@ def UI_box(x, img, color=None, label=None, line_thickness=None):
         tf = max(tl - 1, 1)  # font thickness
         t_size = cv2.getTextSize(label, 0, fontScale=tl / 3, thickness=tf)[0]
 
-        img = draw_border(img, (c1[0], c1[1] - t_size[1] - 3),
-                          (c1[0] + t_size[0], c1[1] + 3), color, 1, 8, 2)
+        img = draw_border(
+            img,
+            (c1[0], c1[1] - t_size[1] - 3),
+            (c1[0] + t_size[0], c1[1] + 3),
+            color,
+            1,
+            8,
+            2,
+        )
 
-        cv2.putText(img, label, (c1[0], c1[1] - 2), 0, tl / 3,
-                    [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
+        cv2.putText(
+            img,
+            label,
+            (c1[0], c1[1] - 2),
+            0,
+            tl / 3,
+            [225, 255, 255],
+            thickness=tf,
+            lineType=cv2.LINE_AA,
+        )
 
 
 def intersect(A, B, C, D):
@@ -202,7 +222,6 @@ def draw_boxes(img, bbox, names, object_id, identities=None, offset=(0, 0)):
             data_deque.pop(key)
 
     for i, box in enumerate(bbox):
-
         x1, y1, x2, y2 = [int(i) for i in box]
         x1 += offset[0]
         x2 += offset[0]
@@ -220,11 +239,11 @@ def draw_boxes(img, bbox, names, object_id, identities=None, offset=(0, 0)):
             data_deque[id] = deque(maxlen=64)
             speed_line_queue[id] = []
         obj_ID = object_id[i]
-        if (obj_ID == 5 or obj_ID == 7):
+        if obj_ID == 5 or obj_ID == 7:
             obj_ID = 2
         color = compute_color_for_labels(obj_ID)
         obj_name = names[obj_ID]
-        label = '{}{:d}'.format("", id) + ":" + '%s' % (obj_name)
+        label = "{}{:d}".format("", id) + ":" + "%s" % (obj_name)
 
         # add center to buffer
         data_deque[id].appendleft(center)
@@ -247,8 +266,12 @@ def draw_boxes(img, bbox, names, object_id, identities=None, offset=(0, 0)):
                     object_counter1[obj_name] += 1
 
         try:
-            label = label + " " + str(sum(speed_line_queue[id]) //
-                                      len(speed_line_queue[id])) + "km/h"  # add speed to label
+            label = (
+                label
+                + " "
+                + str(sum(speed_line_queue[id]) // len(speed_line_queue[id]))
+                + "km/h"
+            )  # add speed to label
         except:
             pass
         UI_box(box, img, label=label, color=color, line_thickness=2)
@@ -260,40 +283,78 @@ def draw_boxes(img, bbox, names, object_id, identities=None, offset=(0, 0)):
             # generate dynamic thickness of trails
             thickness = int(np.sqrt(64 / float(i + i)) * 1.5)
             # draw trails
-            cv2.line(img, data_deque[id][i - 1],
-                     data_deque[id][i], color, thickness)
+            cv2.line(img, data_deque[id][i - 1], data_deque[id][i], color, thickness)
 
-    # 4. Display Count in top right corner
+        # 4. Display Count in top right corner
         for idx, (key, value) in enumerate(object_counter1.items()):
             cnt_str = str(key) + ":" + str(value)
             # print('Number of Vehicles Entering', cnt_str)
             cv2.line(img, (width - 500, 25), (width, 25), [85, 45, 255], 40)
-            cv2.putText(img, f'Number of Vehicles Entering', (width - 500, 35),
-                        0, 1, [225, 255, 255], thickness=2, lineType=cv2.LINE_AA)
-            cv2.line(img, (width - 150, 65 + (idx * 40)),
-                     (width, 65 + (idx * 40)), [85, 45, 255], 30)
-            cv2.putText(img, cnt_str, (width - 150, 75 + (idx * 40)), 0, 1,
-                        [255, 255, 255], thickness=2, lineType=cv2.LINE_AA)
+            cv2.putText(
+                img,
+                f"Number of Vehicles Entering",
+                (width - 500, 35),
+                0,
+                1,
+                [225, 255, 255],
+                thickness=2,
+                lineType=cv2.LINE_AA,
+            )
+            cv2.line(
+                img,
+                (width - 150, 65 + (idx * 40)),
+                (width, 65 + (idx * 40)),
+                [85, 45, 255],
+                30,
+            )
+            cv2.putText(
+                img,
+                cnt_str,
+                (width - 150, 75 + (idx * 40)),
+                0,
+                1,
+                [255, 255, 255],
+                thickness=2,
+                lineType=cv2.LINE_AA,
+            )
 
         for idx, (key, value) in enumerate(object_counter.items()):
             cnt_str1 = str(key) + ":" + str(value)
             # print('Number of Vehicles Leaving', cnt_str1)
 
             cv2.line(img, (20, 25), (500, 25), [85, 45, 255], 40)
-            cv2.putText(img, f'Numbers of Vehicles Leaving', (11, 35), 0, 1,
-                        [225, 255, 255], thickness=2, lineType=cv2.LINE_AA)
-            cv2.line(img, (20, 65 + (idx * 40)),
-                     (127, 65 + (idx * 40)), [85, 45, 255], 30)
-            cv2.putText(img, cnt_str1, (11, 75 + (idx * 40)), 0, 1,
-                        [225, 255, 255], thickness=2, lineType=cv2.LINE_AA)
+            cv2.putText(
+                img,
+                f"Numbers of Vehicles Leaving",
+                (11, 35),
+                0,
+                1,
+                [225, 255, 255],
+                thickness=2,
+                lineType=cv2.LINE_AA,
+            )
+            cv2.line(
+                img, (20, 65 + (idx * 40)), (127, 65 + (idx * 40)), [85, 45, 255], 30
+            )
+            cv2.putText(
+                img,
+                cnt_str1,
+                (11, 75 + (idx * 40)),
+                0,
+                1,
+                [225, 255, 255],
+                thickness=2,
+                lineType=cv2.LINE_AA,
+            )
 
     return img
 
 
 class DetectionPredictor(BasePredictor):
-
     def get_annotator(self, img):
-        return Annotator(img, line_width=self.args.line_thickness, example=str(self.model.names))
+        return Annotator(
+            img, line_width=self.args.line_thickness, example=str(self.model.names)
+        )
 
     def preprocess(self, img):
         img = torch.from_numpy(img).to(self.model.device)
@@ -302,16 +363,17 @@ class DetectionPredictor(BasePredictor):
         return img
 
     def postprocess(self, preds, img, orig_img):
-        preds = ops.non_max_suppression(preds,
-                                        self.args.conf,
-                                        self.args.iou,
-                                        agnostic=self.args.agnostic_nms,
-                                        max_det=self.args.max_det)
+        preds = ops.non_max_suppression(
+            preds,
+            self.args.conf,
+            self.args.iou,
+            agnostic=self.args.agnostic_nms,
+            max_det=self.args.max_det,
+        )
 
         for i, pred in enumerate(preds):
             shape = orig_img[i].shape if self.webcam else orig_img.shape
-            pred[:, :4] = ops.scale_boxes(
-                img.shape[2:], pred[:, :4], shape).round()
+            pred[:, :4] = ops.scale_boxes(img.shape[2:], pred[:, :4], shape).round()
 
         return preds
 
@@ -323,20 +385,22 @@ class DetectionPredictor(BasePredictor):
         self.seen += 1
         im0 = im0.copy()
         if self.webcam:  # batch_size >= 1
-            log_string += f'{idx}: '
+            log_string += f"{idx}: "
             frame = self.dataset.count
         else:
-            frame = getattr(self.dataset, 'frame', 0)
+            frame = getattr(self.dataset, "frame", 0)
 
         self.data_path = p
         # save_path = str(self.save_dir / p.name)  # im.jpg
-        self.txt_path = str(self.save_dir / 'labels' / p.stem) + \
-            ('' if self.dataset.mode == 'image' else f'_{frame}')
-        log_string += '%gx%g ' % im.shape[2:]  # print string
+        self.txt_path = str(self.save_dir / "labels" / p.stem) + (
+            "" if self.dataset.mode == "image" else f"_{frame}"
+        )
+        log_string += "%gx%g " % im.shape[2:]  # print string
         self.annotator = self.get_annotator(im0)
 
+        filter_values = [2, 3, 5, 7]  # filter Car, Motobike, Bus, Truck
         det = preds[idx]
-        det = det[~(det[:, 5] == 0)]
+        det = det[np.isin(det[:, 5], filter_values)]
         if len(det) == 0:
             return log_string
         for c in det[:, 5].unique():
@@ -349,20 +413,25 @@ class DetectionPredictor(BasePredictor):
         confs = []
         oids = []
         for *xyxy, conf, cls in reversed(det):
-            if int(cls) in [2, 3, 5, 7]:
-                xywh_bboxs.append(xyxy_to_xywh(*xyxy))
-                confs.append([conf.item()])
-                oids.append(int(cls))
-        outputs = deepsort.update(torch.Tensor(
-            xywh_bboxs), torch.Tensor(confs), oids, im0)
+            xywh_bboxs.append(xyxy_to_xywh(*xyxy))
+            confs.append([conf.item()])
+            oids.append(int(cls))
+        outputs = deepsort.update(
+            torch.Tensor(xywh_bboxs), torch.Tensor(confs), oids, im0
+        )
         if len(outputs) > 0:
-            draw_boxes(im0, outputs[:, :4], self.model.names,
-                       outputs[:, -1], outputs[:, -2])
+            draw_boxes(
+                im0, outputs[:, :4], self.model.names, outputs[:, -1], outputs[:, -2]
+            )
 
         return log_string
 
 
-@hydra.main(version_base=None, config_path=str(DEFAULT_CONFIG.parent), config_name=DEFAULT_CONFIG.name)
+@hydra.main(
+    version_base=None,
+    config_path=str(DEFAULT_CONFIG.parent),
+    config_name=DEFAULT_CONFIG.name,
+)
 def predict(cfg):
     # init_tracker()
     cfg.model = cfg.model or "yolov8n.pt"
